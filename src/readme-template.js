e416a3da1b7d4f4c4377a078acca00933c87d4c4
Roo <andrew.popio@gmail.com>
2022-09-28 18:58:39 -0400

    started adding additional license badges

--- src/readme-template.js
+++ src/readme-template.js
@@ -1,6 +1,8 @@
 function createBadge(license) {
     if(license === 'Apache') {
         return "[![Apache License, Version 2.0](https://img.shields.io/badge/license-Apache%202.0-green)](https://opensource.org/licenses/Apache-2.0)"
+    } else if (license === 'GNU') {
+        return "[![GNU License]"
     }
 }
 
