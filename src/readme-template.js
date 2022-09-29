96a25a48f929c0188fe0fae343f49650eb5e5cf7
Roo <andrew.popio@gmail.com>
2022-09-28 21:15:22 -0400

    added badges for licenses

--- src/readme-template.js
+++ src/readme-template.js
@@ -2,7 +2,11 @@ function createBadge(license) {
     if(license === 'Apache') {
         return "[![Apache License, Version 2.0](https://img.shields.io/badge/license-Apache%202.0-green)](https://opensource.org/licenses/Apache-2.0)"
     } else if (license === 'GNU') {
-        return "[![GNU License]"
+        return "[![GNU License](https://img.shields.io/badge/license-GNU-green)](https://opensource.org/licenses/GPL-3.0)"
+    } else if (license === 'MIT') {
+        return "[![MIT License](https://img.shields.io/badge/license-MIT-green)](https://opensource.org/licenses/MIT)"
+    } else if (license === 'ISC') {
+        return "[![ISC License](https://img.shields.io/badge/license-ISC-green)](https://opensource.org/licenses/ISC)"
     }
 }
 
